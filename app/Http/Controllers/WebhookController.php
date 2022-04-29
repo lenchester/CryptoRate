41b36cc4c2124596a6dcf7d3f1f6d448c0074719
lenchester <57565463+lenchester@users.noreply.github.com>
2022-04-29 12:24:13 +0200

    fixed webhook controller

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Log
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
 class WebhookController extends Controller
