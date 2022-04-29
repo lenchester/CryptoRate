66382348ebe162d2ba5424bea4a00467d4dcf52b
lenchester <57565463+lenchester@users.noreply.github.com>
2022-04-29 15:36:42 +0200

    added Keyboard, fixed CSRF disabled for webhook

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 use DefStudio\Telegraph\Models\TelegraphChat;
+use DefStudio\Telegraph\Telegraph;
+use DefStudio\Telegraph\Keyboard\Button;
+use DefStudio\Telegraph\Keyboard\Keyboard;
 use Illuminate\Http\Request;
 
 class BotController extends Controller
@@ -9,5 +12,14 @@ class BotController extends Controller
     function sendMessage(){
         $chat = TelegraphChat::find(1);
         $chat->message('hello')->send();
+    }
+
+    function sendKeyboard(){
+        $chat = TelegraphChat::find(1);
+        $chat->message('hello world')
+            ->keyboard(Keyboard::make()->buttons([
+                Button::make('Delete')->action('delete')->param('id', '42'),
+                Button::make('open')->url('https://test.it'),
+            ]))->send();
     }
 }
