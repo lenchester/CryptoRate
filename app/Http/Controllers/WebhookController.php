7b31fbf517f7be6524aa128f9dcab15d160a7543
lenchester <57565463+lenchester@users.noreply.github.com>
2022-04-29 12:20:53 +0200

    added Webhook Controller

---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Log
+use Illuminate\Http\Request;
+
+class WebhookController extends Controller
+{
+    public function index(Request $request){
+        Log::debug($request->all());
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\BotController::class, 'sendMessage']);
+/*Route::get('setwebhook', function(App\Models\User $user){
+    $http  = \Illuminate\Support\Facades\Http::get('https://api.telegram.org/bot5203032567:AAE6r0SsNksa9hlo_mn7aAIf3r7KcL6x3rU/setWebhook?url=https://cryptorate.kz/webhook');
+    dd(json_decode($http->body()));
+});*/
+Route::post('/webhook', [\App\Http\Controllers\WebhookController::class, 'index']);
