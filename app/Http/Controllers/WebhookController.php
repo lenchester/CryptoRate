606e2ad03f9af4a8bf046d02a3850ecb7bfd8bc1
lenchester <57565463+lenchester@users.noreply.github.com>
2022-05-01 22:57:42 +0200

    fix get sender id

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -12,7 +12,7 @@ class WebhookController extends Controller
         $message = $request->input('message');
         Log::debug($message);
         Log::debug($message["from"]);
-        log::debug($message["id"]);
+        log::debug($message["from"]["id"]);
 //        if($text == '/start'){
 //            $this->start($chat_id);
 //        }
