006c9d0dbd49dcafb02652b96b2c740f37223590
lenchester <57565463+lenchester@users.noreply.github.com>
2022-05-12 23:58:47 +0200

    Added CryptoCompare service

---
+++ app/Services/CryptoCompare.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Services;
+use Illuminate\Support\Facades\Http;
+class CryptoCompare
+{
+
+    protected $http;
+    const url = "https://min-api.cryptocompare.com/data";
+    const valid_coins = ['BTC', 'ETH', 'USDT', 'BUSD', 'USDC', 'BNB', 'XRP', 'SOL', 'UST', 'ADA','AVAX', 'TRX', 'DOGE', 'LTC', 'SHIB','DOT', 'LUNA'];
+    const valid_currencies = ['USD', 'EUR', 'KZT'];
+    public function __construct(Http $http)
+    {
+        $this->http = $http;
+    }
+    public function getPrice($coin, $currency){
+        try{
+            assert(in_array($coin, self::valid_coins) && in_array($currency, self::valid_currencies));
+        }
+        catch(InvalidArgumentException $e){
+            return false;
+        }
+        return \GuzzleHttp\json_decode($this->http::get(self::url.'/price?fsym='.$coin.'&tsyms='.$currency)->body(), true)["$currency"];
+    }
+
+}
